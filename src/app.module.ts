3f4fbbc2c5486878fd959548db515777dc0ce007
ckm960411 <a01098956117@gmail.com>
2025-01-25 16:58:12 +0900

    🔧 Config: TypeOrmModule 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService) => ({
+        type: configService.get('DB_TYPE'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
     }),
     PostModule,
   ],
